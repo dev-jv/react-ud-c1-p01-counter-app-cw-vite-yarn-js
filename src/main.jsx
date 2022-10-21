b39129de4ce6e4b663d42ff5e6481581f0273d5b
dev-jv <dev.javierleon@gmail.com>
2022-10-21 08:23:37 -0500

    two

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { CounterApp } from './CounterApp';
+import { FirstApp } from './FirstApp';
+// import { HelloWorldApp } from './HelloWorldApp';
+import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+
+
+ReactDOM.createRoot( document.getElementById('root') ).render(
+    <React.StrictMode>
+        <CounterApp value={ 0 } />
+        {/*<FirstApp title="Hi, I'm Godzilla" />*/}
+    </React.StrictMode>
+);
